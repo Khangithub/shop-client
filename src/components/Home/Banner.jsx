a4f9036bad953ea7087d9783fe6d8a37211fbb06
Mie <dotam1236@gmail.com>
2022-02-03 14:49:09 +0700

    update category list

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -2,15 +2,10 @@ import React from "react";
 import "../_components.scss";
 import { Col, Row, Container, Carousel } from "react-bootstrap";
 
-import ads11 from "../../assets/banners/ads11.jpg";
-import ads12 from "../../assets/banners/ads12.jpg";
-import ads13 from "../../assets/banners/ads13.jpg";
-import ads14 from "../../assets/banners/ads14.jpg";
-import ads15 from "../../assets/banners/ads15.jpg";
-import ads16 from "../../assets/banners/ads16.jpg";
-import ads17 from "../../assets/banners/ads17.jpg";
-import ads18 from "../../assets/banners/ads18.jpg";
-import ads19 from "../../assets/banners/ads19.jpg";
+import homeBanner1 from "../../assets/banners/home-banner-1.jpg";
+import homeBanner2 from "../../assets/banners/home-banner-2.jpg";
+import homeBanner3 from "../../assets/banners/home-banner-3.jpg";
+
 
 function Banner() {
   return (
@@ -19,22 +14,16 @@ function Banner() {
         <Col xs={12}>
           <Carousel>
             {[
-              ads11,
-              ads12,
-              ads13,
-              ads14,
-              ads15,
-              ads16,
-              ads17,
-              ads18,
-              ads19,
+              homeBanner1,
+              homeBanner2,
+              homeBanner3,
             ].map((image, index) => {
               return (
                 <Carousel.Item key={index}>
                   <img
                     className="banner-img d-block w-100"
                     src={image}
-                    alt="First slide"
+                    alt="slide"
                   />
                 </Carousel.Item>
               );
--- src/components/Home/CategoryList.jsx
+++ src/components/Home/CategoryList.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import {Container, Row, Col, Image} from 'react-bootstrap';
+import React from "react";
+import { Container, Row, Col, Image, Carousel } from "react-bootstrap";
+import ReactPlayer from "react-player";
 
-import '../_components.scss';
+import "../_components.scss";
 
-import bookSvg from '../../assets/svgs/book.svg';
-import phoneSvg from '../../assets/svgs/phone.svg';
-import foodSvg from '../../assets/svgs/food.svg';
-import shippingSvg from '../../assets/svgs/shipping.svg';
-import {useHistory} from 'react-router-dom';
+import shippingBanner4 from "../../assets/banners/shipping-4.jpg";
+import shippingBanner3 from "../../assets/banners/shipping-3.jpg";
+import shippingBanner2 from "../../assets/banners/shipping-2.jpg";
+import shippingBanner1 from "../../assets/banners/shipping-1.jpg";
+
+import foodBanner1 from "../../assets/banners/food-1.jpg";
+import foodBanner2 from "../../assets/banners/food-2.jpg";
+import phoneBanner from "../../assets/videos/phone-1.mp4";
+import bookBanner from "../../assets/gifs/book-1.gif";
+
+import { useHistory } from "react-router-dom";
 
 function CategoryList() {
   const history = useHistory();
@@ -16,44 +23,94 @@ function CategoryList() {
     <div className="category">
       <Container className="container-fluid">
         <Row className="category-card-list">
-          {[
-            {name: 'book', icon: bookSvg},
-            {name: 'technology', icon: phoneSvg},
-            {name: 'food', icon: foodSvg},
-          ].map((category, index) => (
-            <Col
-              key={index}
-              xs={12}
-              sm={6}
-              md={3}
-              onClick={() => history.push(`/productList/${category.name}/1`)}
-            >
-              <div className="category-card-container">
-                <h4 className="category-card-name">{category.name}</h4>
-                <Image
-                  src={category.icon}
-                  alt="icon"
-                  className="category-card-image"
-                />
-                <small className="category-card-link">See more</small>
-              </div>
-            </Col>
-          ))}
+          <Col
+            xs={12}
+            sm={6}
+            md={4}
+            onClick={() => history.push(`/productList/book/1`)}
+          >
+            <div className="category-card-container">
+              <Image src={bookBanner} alt="icon" className="category-card-image" />
+              <small className="category-card-link">See more</small>
+            </div>
+          </Col>
+
+          <Col
+            xs={12}
+            sm={6}
+            md={8}
+            onClick={() => history.push(`/productList/phone/1`)}
+          >
+            <div className="category-card-container">
+              <ReactPlayer
+                className="video-ad"
+                url={[
+                  { src: phoneBanner, type: "video/webm" },
+                  { src: phoneBanner, type: "video/ogg" },
+                ]}
+                controls
+                playing
+                muted={true}
+                loop
+              />
+              <small className="category-card-link">See more</small>
+            </div>
+          </Col>
+        </Row>
+
+        <Row className="category-card-list">
+          <Col
+            xs={12}
+            sm={6}
+            md={9}
+            onClick={() => history.push(`/productList/food/1`)}
+          >
+            <div className="category-card-container">
+              <Carousel>
+                {[foodBanner2, foodBanner1].map((image, index) => {
+                  return (
+                    <Carousel.Item key={index}>
+                      <img
+                        className="banner-img d-block w-100"
+                        src={image}
+                        alt="First slide"
+                      />
+                    </Carousel.Item>
+                  );
+                })}
+              </Carousel>
+              <small className="category-card-link">See more</small>
+            </div>
+          </Col>
+
           <Col xs={12} sm={6} md={3}>
             <div className="signin-container">
               <div className="signin-container-signin-btn">
                 <h5>Sign in for the best experience</h5>
-                <button onClick={() => history.push('/login')}>
+                <button onClick={() => history.push("/login")}>
                   Sign in securely
                 </button>
               </div>
 
               <div className="shipping-img">
-                <Image
-                  src={shippingSvg}
-                  alt="shipping-icon"
-                  className="category-img"
-                />
+                <Carousel>
+                  {[
+                    shippingBanner1,
+                    shippingBanner2,
+                    shippingBanner3,
+                    shippingBanner4,
+                  ].map((image, index) => {
+                    return (
+                      <Carousel.Item key={index}>
+                        <img
+                          className="banner-img d-block w-100"
+                          src={image}
+                          alt="First slide"
+                        />
+                      </Carousel.Item>
+                    );
+                  })}
+                </Carousel>
               </div>
             </div>
           </Col>
